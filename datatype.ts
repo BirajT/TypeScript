c2ed5b6a69a8a630d42f0de89f05a40014bbfe42
BirajT <birajtamrakar803@gmail.com>
2025-09-17 10:06:07 +0545

    JS and TS difference

---
+++ datatype.ts
@@ -0,0 +1,14 @@
+// Types:string, number, boolean, null, undefined
+
+//any, unknown, void, never
+let a:number;
+a=20;
+// a="Hello" (it will not accept string)
+let name1: string = "Biraj";
+let age: number = 21;
+let active: boolean = true;
+let balance:null;
+let address:undefined;
+
+console.log("
+    ");
